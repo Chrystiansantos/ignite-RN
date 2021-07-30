69d035f1ccda65bcc6d438b650acaf55398a076e
Chrystian Santos <chrystian.santos@irancho.com.br>
2021-07-29 22:35:40 -0300

    Estilizando a tab-navigation

--- week_2/Anotation.md
+++ week_2/Anotation.md
@@ -569,3 +569,62 @@ return (
   );
 );
 ```
+
+## Estilizando a tab-navigation
+
+Dentro do meu componente de rota consigo passar algumas props para eles para estilizar segue exemplo abaixo:
+
+```tsx
+import React from "react";
+import { useTheme } from "styled-components";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Platform } from "react-native";
+import { Dashboard } from "../Pages/Dashboard";
+import { Register } from "../Pages/Register";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export const AppRoutes = () => {
+  const { colors } = useTheme();
+  return (
+    <Navigator
+      tabBarOptions={{
+        // cor do menu, quando esta selecionado
+        activeTintColor: colors.secondary,
+        // cor quando o elemento esta inativo
+        inactiveTintColor: colors.text,
+        // adiciona um icone ao lado do outro
+        labelPosition: "beside-icon",
+        style: {
+          paddingVertical: Platform.OS === "ios" ? 20 : 10,
+        },
+      }}
+    >
+      <Screen
+        name="Listagem"
+        component={Dashboard}
+        // adicionando um icone ao lado do nome da rota
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
+        }}
+      />
+    </Navigator>
+  );
+};
+```
--- week_2/gofinances/src/routes/app.routes.tsx
+++ week_2/gofinances/src/routes/app.routes.tsx
@@ -1,15 +1,62 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { useTheme } from 'styled-components';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Platform } from 'react-native';
 import { Dashboard } from '../Pages/Dashboard';
 import { Register } from '../Pages/Register';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export const AppRoutes = () => {
+  const { colors } = useTheme();
   return (
-    <Navigator>
-      <Screen name="Listagem" component={Dashboard} />
-      <Screen name="Cadastrar" component={Register} />
+    <Navigator
+      tabBarOptions={{
+        // cor do menu, quando esta selecionado
+        activeTintColor: colors.secondary,
+        // cor quando o elemento esta inativo
+        inactiveTintColor: colors.text,
+        // adiciona um icone ao lado do outro
+        labelPosition: 'beside-icon',
+        style: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 10,
+        },
+      }}
+    >
+      <Screen
+        name="Listagem"
+        component={Dashboard}
+        // adicionando um icone ao lado do nome da rota
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="attach-money" size={size} color={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="pie-chart" size={size} color={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 };
