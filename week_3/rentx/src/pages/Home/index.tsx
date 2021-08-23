42e45dba0ba415b5d20626a2d159fc2f0653179b
Chrystian Santos <chrystian.santos@irancho.com.br>
2021-08-23 17:28:24 -0300

    Criand o Card do carro

--- week_3/rentx/src/pages/Home/index.tsx
+++ week_3/rentx/src/pages/Home/index.tsx
@@ -5,8 +5,20 @@ import { RFValue } from 'react-native-responsive-fontsize';
 import { Container, Header, HeaderContent, TotalCars } from './styles';
 
 import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 export const Home = () => {
+  const car = {
+    brand: 'Mitsubishi',
+    name: 'Lancer',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail:
+      'https://img1.gratispng.com/20171220/egq/mitsubishi-lancer-png-5a3a9535027442.6133742515137887250101.jpg',
+  };
+
   return (
     <Container>
       <StatusBar
@@ -20,6 +32,7 @@ export const Home = () => {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+      <Car data={car} />
     </Container>
   );
 };
