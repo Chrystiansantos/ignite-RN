b5748cfd48d457f673f3c55898beefc60bc03c9a
Chrystian Santos <chrystian.santos@irancho.com.br>
2021-08-24 21:17:53 -0300

    Continuando a interface de detalhes

--- week_3/rentx/src/pages/CarDetails/index.tsx
+++ week_3/rentx/src/pages/CarDetails/index.tsx
@@ -2,7 +2,20 @@ import React from 'react';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 
-import { Container, Header, CarImages } from './styles';
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+} from './styles';
 
 export const CarDetails = () => {
   return (
@@ -17,6 +30,23 @@ export const CarDetails = () => {
           ]}
         />
       </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Mitsubishi</Brand>
+            <Name>Lancer</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 100</Price>
+          </Rent>
+        </Details>
+        <About>
+          Este é automovel desportivo. Surgiu do lendario touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um Belissimo carro
+          paara quem gosta de acelerar
+        </About>
+      </Content>
     </Container>
   );
 };
