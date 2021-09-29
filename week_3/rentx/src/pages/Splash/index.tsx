6394aa937d7678c18997b3f1fb5d8ee9bf4876f7
Chrystian Santos <chrystian.santos@irancho.com.br>
2021-09-28 22:05:07 -0300

    Primeira animação

--- week_3/rentx/src/pages/Splash/index.tsx
+++ week_3/rentx/src/pages/Splash/index.tsx
@@ -1,7 +1,38 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import React from 'react';
+import { Button, StyleSheet } from 'react-native';
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 import { Container } from './styles';
 
 export const Splash = () => {
-  return <Container />;
+  const animation = useSharedValue(0);
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: animation.value }],
+    };
+  });
+
+  const handleAnimationPosition = () => {
+    animation.value = Math.random() * 300;
+  };
+
+  return (
+    <Container>
+      <Animated.View style={[styles.box, animatedStyles]} />
+      <Button title="Mover" onPress={handleAnimationPosition} />
+    </Container>
+  );
 };
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+});
