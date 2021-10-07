e498019fd3333b548c31d845708617d292592817
Chrystian Santos <chrystian.santos@irancho.com.br>
2021-10-07 18:32:22 -0300

    Gestos do usuario

--- week_3/rentx/src/pages/Home/index.tsx
+++ week_3/rentx/src/pages/Home/index.tsx
@@ -1,17 +1,19 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import React, { useEffect, useState } from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, StyleSheet } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation, CommonActions } from '@react-navigation/native';
 import { useTheme } from 'styled-components/native';
-import {
-  Container,
-  Header,
-  HeaderContent,
-  TotalCars,
-  CarList,
-  MyCarsButton,
-} from './styles';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+  withSpring,
+} from 'react-native-reanimated';
+import { RectButton, PanGestureHandler } from 'react-native-gesture-handler';
+
+import { Container, Header, HeaderContent, TotalCars, CarList } from './styles';
 
 import Logo from '../../assets/logo.svg';
 import { Car } from '../../components/Car';
@@ -19,12 +21,39 @@ import { api } from '../../service/api';
 import { ICarDTO } from '../../dtos/ICarDTO';
 import { Load } from '../../components/Load';
 
+const ButtonAnimated = Animated.createAnimatedComponent(RectButton);
+
 export const Home = () => {
   const { navigate, dispatch } = useNavigation();
   const { colors } = useTheme();
 
   const [cars, setCars] = useState<ICarDTO[]>([]);
   const [loading, setLoading] = useState(true);
+
+  const positionY = useSharedValue(0);
+  const positionX = useSharedValue(0);
+
+  const myCarsButtonStyle = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: positionX.value },
+      { translateY: positionY.value },
+    ],
+  }));
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart(_, ctx: any) {
+      ctx.positionX = positionX.value;
+      ctx.positionY = positionY.value;
+    },
+    onActive(event, ctx: any) {
+      positionX.value = event.translationX + ctx.positionX;
+      positionY.value = event.translationY + ctx.positionY;
+    },
+    onEnd() {
+      positionX.value = withSpring(0);
+      positionY.value = withSpring(0);
+    },
+  });
 
   useEffect(() => {
     const loadData = async () => {
@@ -83,9 +112,35 @@ export const Home = () => {
           )}
         />
       )}
-      <MyCarsButton onPress={handleOpenMyCars}>
-        <Ionicons name="ios-car-sport" size={32} color={colors.shape} />
-      </MyCarsButton>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View
+          style={[
+            myCarsButtonStyle,
+            {
+              position: 'absolute',
+              bottom: 13,
+              right: 22,
+            },
+          ]}
+        >
+          <ButtonAnimated
+            onPress={handleOpenMyCars}
+            style={[styles.button, { backgroundColor: colors.main }]}
+          >
+            <Ionicons name="ios-car-sport" size={32} color={colors.shape} />
+          </ButtonAnimated>
+        </Animated.View>
+      </PanGestureHandler>
     </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
