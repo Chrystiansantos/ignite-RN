26865945921acfb3d3d300e0be688ac719bffab3
Chrystian Santos <chrystian.santos@irancho.com.br>
2021-12-31 09:30:44 -0300

    Mocks personalizados

--- week_2/gofinances/src/__test__/hooks/auth.spec.tsx
+++ week_2/gofinances/src/__test__/hooks/auth.spec.tsx
@@ -3,41 +3,57 @@ import fetchMock from 'jest-fetch-mock';
 import { renderHook, act } from '@testing-library/react-hooks';
 
 import * as mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import { mocked } from 'ts-jest/utils';
+import { startAsync } from 'expo-auth-session';
 import { AuthProvider, useAuth } from '../../hooks/AuthContext';
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
-// jest.mock('expo-auth-session');
-jest.mock('expo-auth-session', () => {
-  return {
-    startAsync: () => ({
+jest.mock('expo-auth-session');
+
+// Coloque no inicio do arquivo para habilitar o mock do fetch.
+fetchMock.enableMocks();
+describe('Auth hook', () => {
+  it('user should be not connect if cancel authentication with Google', async () => {
+    const googleMocked = mocked(startAsync as any);
+    googleMocked.mockReturnValue({
+      type: 'cancel',
+    });
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    await act(() => result.current.signInWithGoogle());
+    expect(result.current.user).not.toHaveProperty('id');
+  });
+
+  it('should be able to sign in with Google account existing', async () => {
+    const googleMocked = mocked(startAsync as any);
+    googleMocked.mockReturnValue({
       type: 'success',
       params: {
         access_token: 'any_token',
       },
-    }),
-  };
-});
+    });
 
-// Coloque no inicio do arquivo para habilitar o mock do fetch.
-fetchMock.enableMocks();
-it('should be able to sign in with Google account existing', async () => {
-  // Agora que temos o Token, vamos mockar a requisição ttp dos dados de profile do usuário.
-  fetchMock.mockResponseOnce(
-    JSON.stringify({
-      id: 'any_id',
-      email: 'chrystian@gmail.com',
-      name: 'Chrystian',
-      photo: 'any_photo.png',
-    }),
-  );
+    // Agora que temos o Token, vamos mockar a requisição ttp dos dados de profile do usuário.
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        id: 'any_id',
+        email: 'chrystian@gmail.com',
+        name: 'Chrystian',
+        photo: 'any_photo.png',
+      }),
+    );
 
-  const { result } = renderHook(() => useAuth(), {
-    wrapper: AuthProvider,
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    // eslint-disable-next-line no-return-await
+    await act(async () => await result.current.signInWithGoogle());
+    // await waitForNextUpdate();
+
+    expect(result.current.user.email).toBe('chrystian@gmail.com');
   });
-
-  // eslint-disable-next-line no-return-await
-  await act(async () => await result.current.signInWithGoogle());
-  // await waitForNextUpdate();
-
-  expect(result.current.user.email).toBe('chrystian@gmail.com');
 });
